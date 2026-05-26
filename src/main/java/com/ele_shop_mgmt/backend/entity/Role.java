43358e923151f493f32a578487ae1393689ed518
PotdarSumit <spotdar1001@gmail.com>
2026-05-26 18:59:28 +0530

    Initial commit with spring boot project

---
+++ src/main/java/com/ele_shop_mgmt/backend/entity/Role.java
@@ -0,0 +1,6 @@
+package com.ele_shop_mgmt.backend.entity;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_STAFF
+}
---
+++ src/test/java/com/ele_shop_mgmt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ele_shop_mgmt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
